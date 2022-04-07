2258e95a58338cb07a171f6f5bd19c5757652770
kruumy <48849543+kruumy@users.noreply.github.com>
2022-04-07 03:11:00 +0000

    Add files via upload

---
+++ START_IW3xo.bat
@@ -0,0 +1,7 @@
+@echo OFF
+title windowed mode iw3xo
+color 0a
+
+START iw3xo.exe +set r_aspectRatio custom +set r_customMode 1920x1080 +set r_aspectRatio_custom 1.7777  +set r_fullscreen 0 +set r_noborder 1 +set load_iw3mvm 1
+
+pause
