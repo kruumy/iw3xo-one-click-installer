18e0d9e98269662c0d3f2f14287114e132544cd0
kruumy <48849543+kruumy@users.noreply.github.com>
2022-04-07 03:08:38 +0000

    Add files via upload

--- iw3xoinstaller.bat
+++ iw3xoinstaller.bat
@@ -15,13 +15,22 @@ fltmc >nul 2>&1 || (
 cd %~dp0
 if exist "%LOCALAPPDATA%"\CallofDuty4MW (  
 	echo Please Make sure you have COD4X deleted and uninstalled before installing
-	pause
-	cls
 )
+
+pause
+cls
+
+if exist iw3xo.exe (
+	echo It seems you have IW3xo installed already...
+	echo We found that this directroy has the iw3xo.exe
+)
+
+pause
+cls
+
 echo Current Cod4 directory: %~dp0
 echo Is this OK? If not make sure the installer is in the COD4 Directory
 pause
-cls
 
 powershell -Command "Invoke-WebRequest https://github.com/xoxor4d/iw3xo-dev/releases/download/3428/iw3xo_b3428.zip -OutFile iw3xo_b3428.zip"
 powershell -Command "Expand-Archive -LiteralPath iw3xo_b3428.zip"
